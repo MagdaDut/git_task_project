bc9d64b0a316b09cc851ecb66551eaeb04abc1b4
MagdaDut <hypsofteng@gmail.com>
2024-05-10 10:21:15 +0200

    Added some comments to my code

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
+'''
+    This method will return a greeting
+    input: the users message
+'''
 def greeting(message):
     print(message)
 
@@ -5,4 +9,6 @@ print("Hello World!")
 print("Git is awesomeness!")
 
 user_input = input("Enter the most amazing greeting: ")
+
+# Display the users amazing message
 greeting(user_input)
