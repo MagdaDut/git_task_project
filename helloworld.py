e5a5b3da794fdfdc78cf7dbf0d6f9a23e7e1a230
MagdaDut <hypsofteng@gmail.com>
2024-05-14 09:21:44 +0200

    Adding a pretty greeting on a card

--- helloworld.py
+++ helloworld.py
@@ -2,13 +2,18 @@
     This method will return a greeting
     input: the users message
 '''
-def greeting(message):
-    print(message)
-
+def greeting_card(message):
+    print("******************************************")
+    print("*                                        *")
+    print("*                                        *")
+    print("*" + message + "*")
+    print("*                                        *")
+    print("*                                        *")
+    print("******************************************") 
 print("Hello World!")
 print("Git is awesomeness!")
 
 user_input = input("Enter the most amazing greeting: ")
 
 # Display the users amazing message
-greeting(user_input)
+greeting_card(user_input)
